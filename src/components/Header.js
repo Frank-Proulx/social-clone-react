debe0ad944b5f88e8d54094c6a6fdfb027011f61
Frank-Proulx <frankproulx7@gmail.com>
2022-02-07 13:22:30 -0800

    add boilerplate components

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from './Navbar';
+
+function Header(){
+  return (
+    <React.Fragment>
+      <Navbar />
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+export default Header;
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar(){
+  return (
+    <React.Fragment>
+      <h1>Home</h1>
+      <h1>Notifications</h1>
+      <h1>Messages</h1>
+    </React.Fragment>
+  );
+}
+
+export default Navbar;
